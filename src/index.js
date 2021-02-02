8e36e967a4e95435c94ea04508bcf0450e7c6f59
vova <vova.shkar74@gmail.com>
2021-02-02 13:56:26 +0300

    feat: makes func brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // Создаем массив из аргумента bracketsConfig
+  const arr = [];
+  let i = 0;
+  bracketsConfig.forEach(elem => {
+    arr.push(elem.join(''));
+    });
+    console.log(arr)
+  // Цикл для проверки символа строки str на наличие в массиве
+  while (i < arr.length) {
+    if (str.includes(arr[i])) {
+      str = str.replace(arr[i], '') 
+      i = 0} else i++;
+  }
+  return (str.length === 0) ? true : false;
 }
